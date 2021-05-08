6bed9d335a6e29d275257017def5af08dd8058eb
Dương Thành Vương <vuong.dt.23@gmail.com>
2021-05-08 12:04:52 +0700

    firebase intergration

---
+++ Swoletarian/src/Firebase.js
@@ -0,0 +1,17 @@
+import  firebase from 'firebase';
+
+const settings = {timestampsInSnapshots: true};
+
+const config = {
+ apiKey: "AIzaSyDkCbIolNP2gV-5NkiovHG_JFq49MlxvFo",
+    authDomain: "swoletarian.firebaseapp.com",
+    databaseURL: "https://swoletarian-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "swoletarian",
+    storageBucket: "swoletarian.appspot.com",
+    messagingSenderId: "917394744534",
+    appId: "1:917394744534:web:fa874271bcd3edecff1577",
+    measurementId: "G-BEPGC7Y5F2"
+};
+firebase.initializeApp(config);
+
+export default firebase;
